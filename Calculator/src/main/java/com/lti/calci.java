04190956337498d63631799533e3aa0e4334739d
namrata-kumar <kumarnamrata529@gmail.com>
2020-12-21 12:40:40 +0530

    Added calci from eclipse

---
+++ Calculator/src/main/java/com/lti/calci.java
@@ -0,0 +1,11 @@
+package com.lti;
+
+public class calci {
+	public static int add(int num1,int num2) {
+		return num1+num2;
+	}
+	public static void main(String []args) {
+		System.out.println("Addition= "+add(10,20) );
+	}
+
+}
