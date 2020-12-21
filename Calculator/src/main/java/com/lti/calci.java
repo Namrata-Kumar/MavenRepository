8ef260391a0e172a62082c71cd33686246479216
namrata-kumar <kumarnamrata529@gmail.com>
2020-12-21 14:58:47 +0530

    Added methods

--- Calculator/src/main/java/com/lti/calci.java
+++ Calculator/src/main/java/com/lti/calci.java
@@ -4,8 +4,20 @@ public class calci {
 	public static int add(int num1,int num2) {
 		return num1+num2;
 	}
+	public static int sub(int num1,int num2) {
+		return num1-num2;
+	}
+	public static int mul(int num1,int num2) {
+		return num1*num2;
+	}
+	public static int div(int num1,int num2) {
+		return num1/num2;
+	}
 	public static void main(String []args) {
 		System.out.println("Addition= "+add(10,20) );
+		System.out.println("Diffrence= "+sub(10,20) );
+		System.out.println("Multiple= "+mul(10,20) );
+		System.out.println("Division= "+div(10,20) );
 	}
 
 }
